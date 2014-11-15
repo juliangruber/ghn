b9ba97471e4fd36558db259175a693a512571804
Julian Gruber <julian@juliangruber.com>
2014-11-15 11:42:47 +0100

    list of notifications

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# ghn
+
+  GitHub notifications cli
+
+## Example
+
+```js
+var ghn = require('ghn');
+
+
+```
+
+## Installation
+
+```bash
+$ npm install ghn
+```
+
+## License
+
+  MIT
+
---
+++ index.js
@@ -0,0 +1,22 @@
+var request = require('hyperquest');
+var JSONStream = require('JSONStream');
+var through = require('through2');
+
+var token = process.env.TOKEN;
+
+var req = request('https://api.github.com/notifications');
+req.setHeader('Authorization', 'token ' + token);
+req.setHeader('User-Agent', 'https://github.com/juliangruber/ghn');
+
+req
+.pipe(JSONStream.parse('*'))
+.pipe(renderNotification())
+.pipe(process.stdout);
+
+function renderNotification(){
+  var idx = 0;
+  return through.obj(function(n, _, done){
+    this.push(idx++ + ') ' + n.repository.full_name + ': ' + n.subject.title + '\n');
+    done();
+  });
+}
